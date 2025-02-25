3c318df6e00e941febf8e5cb0b52dcc6dcc70f5f
paravena <adla_paravena@contractor.indeed.com>
2025-02-24 22:14:47 -0300

    updating railway configuration

--- app/main.py
+++ app/main.py
@@ -59,4 +59,5 @@ def health_check():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    port = int(os.getenv('PORT', 3000))
+    app.run(host='0.0.0.0', port=port, debug=True)
